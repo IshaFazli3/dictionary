0ad3ee4cdb4f6a3a2bb613a951bf6ba7f9322917
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-09-26 18:53:14 -0500

    Rendered Meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary(){
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState({});
+
 
   function handleResponse(response){
     console.log(response.data);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
@@ -14,6 +18,7 @@ export default function Dictionary(){
     let apiUrl = 
     "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
     console.log(response.data[0]);
+    setResults(response.data[0]);
     
     axios.get(apiUrl).then(handleResponse);
   }
@@ -29,6 +34,7 @@ export default function Dictionary(){
       <input type="search" 
       onChange={handleKeywordChange} />
      </form>
+     <Results results={results}/>
     </div>
   ) 
 }
