cf212e81ca3793b02bc1d113091b449c450fbb01
Isha F <137927479+IshaFazli3@users.noreply.github.com>
2023-09-27 14:25:35 -0500

    Made some corrections

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,40 +1,33 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
 
-export default function Dictionary(){
-  let [keyword, setKeyword] = useState("");
-  let [results, setResults] = useState({});
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
 
-
-  function handleResponse(response){
+  function handleResponse(response) {
     console.log(response.data);
     setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
-    
-    let apiUrl = 
-    "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
-    console.log(response.data[0]);
-    setResults(response.data[0]);
-    
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
-
-  function handleKeywordChange(event){
+  function handleKeywordChange(event) {
     setKeyword(event.target.value);
-
   }
+
   return (
     <div classNme="Dictionary">
-     <form onSubmit={search}> 
-      <input type="search" 
-      onChange={handleKeywordChange} />
-     </form>
-     <Results results={results}/>
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      {results ? <Results results={results} /> : null}
     </div>
-  ) 
+  );
 }
